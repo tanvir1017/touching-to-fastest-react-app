ddea80e5c0f1eecace21bda1935345d706b3d30d
tanvir1017 <tanvir.hossaindev1@gmail.com>
2023-01-26 22:16:21 +0000

    perf: ⚡️ Testing the performance for the new fastest react app
    Testing the performance for the new fastest react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Hello from Tanu1017</h1>
+      <p>Checking the fastness from the react+vite</p>
+    </div>
+  );
+}
+
+export default App;
